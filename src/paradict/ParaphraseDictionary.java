8107c41c19fe69f64a82b28ce8d9a34b39469322
nicklin <1500016611@pku.edu.cn>
2018-09-29 07:02:37 +0800

    modification for new data, some futher debugging

--- src/paradict/ParaphraseDictionary.java
+++ src/paradict/ParaphraseDictionary.java
@@ -37,7 +37,7 @@ public class ParaphraseDictionary {
 	public HashSet<String> bannedTypes;
 	
 	//public final int typePredicateID = 1541;	//dbpedia2015 <type>=1541
-	public final int typePredicateID = 6042;	//Dbpedia 2014 <type>=6042
+	public final int typePredicateID = 5166;	//Dbpedia 2016 <type>=5166
 	public int totalPredCount = 0;
 	public int paraphrasedPredCount = 0;
 	public int lineCount = 0;
